a1a55a2b5bcc9ebaa22ecd509b1adca23cdec1ce
Rebecca Strauss <rstrauss127@gmail.com>
2017-11-30 23:18:06 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(index)
+  index = index.to_i - 1
+end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && (board[index] == "" || board[index] == " " || board[index] == nil)
+end
+
+
+def move(board, index, token = "X")
+  board[index] = token
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  index = gets.strip
+  index = input_to_index(index)
+  if valid_move?(board, index)
+    move(board, index, token = "X")
+    display_board(board)
+  else
+    puts "Please enter 1-9:"
+    index = gets.strip
+    index = input_to_index(index)
+  end
+
+end
